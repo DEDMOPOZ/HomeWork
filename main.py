212457ab3cefc747456bd160bb8070350bc86f3b
Vyacheslav <dedmopoz.bakemono@gmail.com>
2021-03-16 00:18:01 +0200

    parse_cookie_second

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def parse_cookie(query: str) -> dict:
-    return {}
+    list = query.split(";")
+    tmp_dict = {}
+    for i in list:
+        tmp_list = i.split("=", 1)
+        try:
+            tmp_dict.update({tmp_list[0]:tmp_list[1]})
+        except IndexError:
+            break
+    else: return {}
 
+    return tmp_dict
 
 if __name__ == '__main__':
     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
