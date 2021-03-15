bb7c3f4d4491fc6925777b370bca65119d98a93d
Vyacheslav <dedmopoz.bakemono@gmail.com>
2021-03-16 01:03:26 +0200

    parce_function_second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def parse(query: str) -> dict:
-        list = query.split(";")
+        try:
+            list = (query.split("?")[1]).split("&")
+        except IndexError:
+            return {}
         tmp_dict = {}
         for i in list:
             tmp_list = i.split("=", 1)
@@ -7,11 +10,8 @@ def parse(query: str) -> dict:
                 tmp_dict.update({tmp_list[0]: tmp_list[1]})
             except IndexError:
                 break
-        else:
-            return {}
 
         return tmp_dict
-
 
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
