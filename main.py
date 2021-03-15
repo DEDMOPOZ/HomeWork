47d045d4f70a0f440db42be0a5410002c467248f
Vyacheslav <dedmopoz.bakemono@gmail.com>
2021-03-16 00:40:04 +0200

    new

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 def parse(query: str) -> dict:
-    return {}
+        list = query.split(";")
+        tmp_dict = {}
+        for i in list:
+            tmp_list = i.split("=", 1)
+            try:
+                tmp_dict.update({tmp_list[0]: tmp_list[1]})
+            except IndexError:
+                break
+        else:
+            return {}
+
+        return tmp_dict
 
 
 if __name__ == '__main__':
