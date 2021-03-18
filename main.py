069f39b0a3dfaca79690c0e7e4c8472ed8d537eb
Vyacheslav <dedmopoz.bakemono@gmail.com>
2021-03-18 20:25:41 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-def parse(query: str) -> dict:
-        from urllib import parse
-        tmp_dict = {}
-        tmp_dict.update(parse.parse_qsl(parse.urlparse(query)[4]))
-        return tmp_dict
 
-
-if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
